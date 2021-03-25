ff73c741f773da05737b1385675fd1e7ffec52d3
Lisa Dannegger <lisa.dannegger@hotmail.de>
2021-03-25 17:53:54 +0100

    Too many changes

--- src/js/contactForm.js
+++ src/js/contactForm.js
@@ -1,95 +1,35 @@
+const contactForm = document.getElementById("contact-form");
 
-// let myForm = document.getElementById("contact-form");
-// let userName = document.getElementById("input-username");
-// myForm.addEventListener('submit', function (event) {
-//     let error = false;
+function validateForm() {
+  const nameInput = document.getElementById("userName");
+  const emailInput = document.getElementById("userEmail");
+  const checkbox = document.getElementById("checkbox");
 
-//     if(error)   {
-//         event.preventDefault();
-//         userName.focus();
-//         return false;
+  if (nameInput.value === "") {
+    nameInput.classList.add("invalid");
+    console.log("läuft");
+    return false;
+  }
+  if (emailInput.value === "") {
+    emailInput.classList.add("invalid");
+    console.log("Jo");
+    return false;
+  }
 
-//     }
-// });
+  if (emailInput.contains("@") === false) {
+    emailInput.classList.add("invalid");
+    return false;
+  }
 
-//Jedes Feld überprüfen: Was ins Feld eingegeben wird, wird über den value bestimmt. Stimmt der Value
-//mit der Eingabe des Users nicht überein mach das (färbe den Background rot, gib eine Fehlermeldung aus). 
-//Wenn er übereinstimmt, färbe den Background grün, springe ins nächste Feld.
-
-// const form = document.getElementById("contact-form");
-
-// for (const i = 0; i < form.clientHeight; i++)   {
-//     form[i].addEventListener('invalid', function(e) {
-//         e.preventDefault();
-//     }, true);
-// }
-
-
-
-
-
-// function validation()   {
-
-//     var userName = document.getElementById("input-username");
-//     var userMail = document.getElementById("input-usermail");
-
-
-//     if (userName.value == "")    {
-//         userName.style.backgroundColor = rgb(255, 0, 0);
-//     }
-
-//     if(userMail.value == "")    {
-//         userName.style.backgroundColor = rgb(255, 0, 0);
-    
-    
-//     } else {
-//         //Hintergrund-Farbe grün
-//     }
-// }
-
-// validation();
-
-const contactForm = document.querySelector("form");
-
-
-//Leere Felder prüfen
-function validateForm()  {
-    const nameInput = document.getElementById("userName");
-    const emailInput = document.getElementById("userEmail");
-    const checkbox = document.getElementById("checkbox");
-
-    if(nameInput.value == "") {
-        alert("Please enter your Username");
-        return false;
-    }
-    if(emailInput.value == "") {
-        alert("Please enter you Email");
-        return false;
-    }
-
-    if(emailInput.contains("@") === false)  {
-        alert("Please enter a valid Emailadress");
-    }
-
-    if(checkbox.checked === false)  {
-        alert("Please acceept the Privacy Link");
-    }
-
-    alert("All datas are valid!, send it to the server!")
-
-    return true;
+  if (checkbox.checked === false) {
+    alert("Please acceept the Privacy Link");
+    return false;
+  }
+  return true;
 }
 
-
-document.getElementById('contact-form').addEventListener ("submit", function (evt) {
-    // Eingaben nicht korrekt
-    evt.preventDefault("FehlendeEingabe");
-    //
+contactForm.addEventListener("submit", function (evt) {
+  evt.preventDefault("FehlendeEingabe");
 });
 
-
 export default validateForm;
-
-
-
-
--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,31 @@
-import shop from "./shop";
-shop();
-
-import shopDetail from "./shopDetail";
-shopDetail();
-
-// import contactForm from "./contactForm";
-// validateForm();
-
+// import shop from "./shop";
+import validateForm from "./contactForm";
 import navigation from "./navigation";
+
 navigation();
+
+const navBar = document.querySelector(".main-navigation-box");
+const divBar = document.querySelector(".main-navigation");
+
+window.onscroll = function () {
+  const top = window.scrollY;
+
+  if (top <= 450) {
+    navBar.classList.remove("nav-scrolled");
+    divBar.classList.remove("div-scrolled");
+  } else {
+    navBar.classList.add("nav-scrolled");
+    divBar.classList.add("div-scrolled");
+  }
+};
+
+validateForm();
+
+// shop();
+
+// import shopDetail from "./shopDetail";
+
+// shopDetail();
 
 // import validation from "./contactForm";
 // validation();
